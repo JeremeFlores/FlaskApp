e081cbf7b61cb05b1ddfb19d5a0ee405f965cf32
JeremeFlores <flores.jereme@cec.edu.ph>
2025-04-29 21:52:40 +0800

    prepare

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello from Flask on Railway!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
